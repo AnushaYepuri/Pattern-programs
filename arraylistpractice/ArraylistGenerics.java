e2339a9a5f1745afda8055fa539204b943b36a65
AnushaYepuri <anushayepuri500@gmail.com>
2026-05-28 14:24:07 +0530

    Updated ArrayList Generics

--- arraylistpractice/ArraylistGenerics.java
+++ arraylistpractice/ArraylistGenerics.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class ArraylistGenerics {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arr = new ArrayList<>(10);
+        arr.add(1);
+        arr.add(2);
+        arr.add(1);
+        arr.add(2);
+        System.out.println(arr);
+        arr.add(3,7);
+        arr.add(4,0);
+        System.out.println(arr);
+        arr.set(2,10);
+        arr.set(3,20);
+        System.out.println(arr);
+        arr.remove(2);
+
+        System.out.println(arr);
+        System.out.println(arr.get(4));
+
+    }
 }
