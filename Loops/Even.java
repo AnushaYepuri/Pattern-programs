1b2c61e2c2c6f7ee4cf914cfb0fb0e4a0581f68a
AnushaYepuri <anushayepuri500@gmail.com>
2026-06-05 06:25:27 +0530

    Learing patterns

---
+++ Loops/Even.java
@@ -0,0 +1,9 @@
+public class Even {
+    public static void main(String[] args) {
+        for(int a = 1; a <= 100; a++){
+            if(a%2==0){
+                System.out.println(a);
+            }
+        }
+    }
+}
---
+++ Loops/ReverseNumbers.java
@@ -0,0 +1,8 @@
+public class ReverseNumbers {
+    public static void main(String[] args) {
+
+        for(int a = 10; a >= 1; a--){
+            System.out.println(a);
+        }
+    }
+}
